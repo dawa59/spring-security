cc2be3d935ebed035cca9bcff4df3fed9fd257f2
Andres Perez <perezlopezandres85@gmail.com>
2024-06-17 11:07:46 +0200

    Configuración de Cors

--- src/main/java/com/cursoSecurity/app_security/security/SecurityConfig.java
+++ src/main/java/com/cursoSecurity/app_security/security/SecurityConfig.java
@@ -12,52 +12,77 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 /**
  * Configuración estandar, no es necesario ponerla
  */
 @Configuration
 public class SecurityConfig {
-    @Bean
-        //sino uso esta anotación nunca se va a añadir al contenedor de Spring
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/loans", "/balance", "/account", "/cards")
-                                .authenticated()
-                                .anyRequest().permitAll()) //cualquier request mandada tiene que tener autenticación
-                .formLogin(Customizer.withDefaults()) // formato del login
-                .httpBasic(Customizer.withDefaults()); // tipo de autonticación http, usuario y contraseña
+  @Bean
+    //sino uso esta anotación nunca se va a añadir al contenedor de Spring
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(auth ->
+                    auth.requestMatchers("/loans", "/balance", "/account", "/cards")
+                            .authenticated()
+                            .anyRequest().permitAll()) //cualquier request mandada tiene que tener autenticación
+            .formLogin(Customizer.withDefaults()) // formato del login
+            .httpBasic(Customizer.withDefaults()); // tipo de autonticación http, usuario y contraseña
+    http.cors(cors -> corsConfigurationSource());
+    return http.build();
+  }
 
-        return http.build();
-    }
-/** se comenta por que se va usar los datos de la base de datos
- *
-    @Bean
-    InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        UserDetails admin = User.withUsername("admin")
-                .password("to_be_enconded")
-                .authorities("ADMIN")
-                .build();
+  /**
+   * se comenta por que se va usar los datos de la base de datos
+   *
+   * @Bean InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+   * UserDetails admin = User.withUsername("admin")
+   * .password("to_be_enconded")
+   * .authorities("ADMIN")
+   * .build();
+   * <p>
+   * UserDetails user = User.withUsername("user")
+   * .password("to_be_enconded")
+   * .authorities("USER")
+   * .build();
+   * <p>
+   * return new InMemoryUserDetailsManager(admin, user);
+   * }
+   * @Bean UserDetailsService userDetailsService(DataSource dataSource){
+   * return new JdbcUserDetailsManager(dataSource);
+   * }
+   */
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    //return  NoOpPasswordEncoder.getInstance();
+    return NoOpPasswordEncoder.getInstance();
+  }
 
-        UserDetails user = User.withUsername("user")
-                .password("to_be_enconded")
-                .authorities("USER")
-                .build();
+  /**
+   * Método de configuración de cors en el que solo se permite los metodos get y post
+   *
+   * @return
+   */
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration config = new CorsConfiguration();
+    //Url especificas permitidas
+    // config.setAllowedOrigins(List.of("http://localhost:8080/"));
+    config.setAllowedOrigins(List.of("*"));
+    config.setAllowedOrigins(List.of("GET", "POST"));
+    config.setAllowedMethods(List.of("*"));
+    config.setAllowedHeaders(List.of("*"));
 
-        return new InMemoryUserDetailsManager(admin, user);
-    }
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    //proteger
+    source.registerCorsConfiguration("/**", config);
+    return source;
 
-    @Bean
-    UserDetailsService userDetailsService(DataSource dataSource){
-        return new JdbcUserDetailsManager(dataSource);
-    }
- */
-    @Bean
-    PasswordEncoder passwordEncoder(){
-        //return  NoOpPasswordEncoder.getInstance();
-      return  NoOpPasswordEncoder.getInstance();
-    }
+  }
 
 }
