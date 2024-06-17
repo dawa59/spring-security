a31e2cac8ec534082bbaf0a67966e87de77efeab
Andres Perez <perezlopezandres85@gmail.com>
2024-06-17 12:24:28 +0200

    Creación de tablas de customers y roles

--- db/sql/data.sql
+++ db/sql/data.sql
@@ -1,3 +1,12 @@
-insert into customers (email, pwd, rol) VALUES
-  ('super_user@debuggeandoieas.com', 'to_be_encoded', 'admin'),
-  ('basic_user@debuggeandoieas.com', 'to_be_encoded', 'user');
+
+insert into customers(email, pwd) values
+    ('account@debuggeando.com', 'to_be_encoded'),
+    ('cards@debuggeando.com', 'to_be_encoded'),
+    ('loans@debuggeando.com', 'to_be_encoded'),
+    ('balance@debuggeando.com', 'to_be_encoded')
+
+insert into roles (role_name, description, id_customer) values
+    ('VIEW_ACCOUNT', 'cant view account endpoint', 1),
+    ('VIEW_CARDS', 'cant view account endpoint', 2),
+    ('VIEW_LOANS', 'cant view account endpoint', 3),
+    ('VIEW_BALANCE', 'cant view account endpoint', 4),
