806fd5b7427be7913de50f3fb4623ae13eee6434
Andres Perez <perezlopezandres85@gmail.com>
2024-07-01 13:06:25 +0200

    Borrado para configurar el OAUTH-2

--- src/main/java/com/cursoSecurity/app_security/AppSecurityApplication.java
+++ src/main/java/com/cursoSecurity/app_security/AppSecurityApplication.java
@@ -2,11 +2,10 @@ package com.cursoSecurity.app_security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 
 @SpringBootApplication
-/*la anotación EnableWebSecurity no es obligatoria en esta versión. Ahora viene por defecto*/
-@EnableWebSecurity
+
 public class AppSecurityApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/cursoSecurity/app_security/entities/JWTRequest.java
+++
@@ -1,9 +0,0 @@
-package com.cursoSecurity.app_security.entities;
-
-import lombok.Data;
-
-@Data
-public class JWTRequest {
-  private String userName;
-  private String passWord;
-}
